3acbe826fb36c2535d3b88a93146dd0e384c8ca1
ArturBaranov <abear.249@gmail.com>
2025-07-14 00:56:00 +0300

    add solving input when input not numbers

--- src/utils/ScannerUI.java
+++ src/utils/ScannerUI.java
@@ -1,13 +1,23 @@
 package utils;
 
+import exceptions.InvalidNumberException;
+
 import java.util.Scanner;
 
 public class ScannerUI {
 
     private Scanner scanner = new Scanner(System.in);
 
-    public int userChoice(){
-        return Integer.parseInt(scanner.nextLine());
+    public int userChoice() throws NumberFormatException {
+        while (true) {
+            try {
+                String userChoice = scanner.nextLine();
+                return Integer.parseInt(userChoice);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number format");
+            }
+
+        }
     }
 
     public String scanUserName() {
